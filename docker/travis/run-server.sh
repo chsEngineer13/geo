54c50942239b28d9cb3371ceb773dee58f81c2cd
Daniel Berry <dberry@boundlessgeo.com>
2016-10-21 11:34:21 -0500

    Use JDK instead of JRE, add apache proxy, fix requirements.txt (#91)
    - modified Travis to use the jdk, instead of the jre. (not sure why, but it just seemed to work. My guess is that the args that were being passed required jdk) 
    - modifed geoserver geonode authprovider to use port 80, instead of 8000 (adding a proxy to allow for geoserver and geonode to reside on the same domain, needed for auth cookie)
    - added apache2 to travis
    - added header and proxy mods to apache2
    - added config for reverse proxy for 8000 and 8080
    - added upload_test.py (currently set to off, due to a timeout during the actual selenium upload)

--- docker/travis/run-server.sh
+++ docker/travis/run-server.sh
@@ -3,4 +3,4 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )";
 source $DIR/exchange-settings.sh;
 
-/env/bin/python /opt/boundless/exchange/manage.py runserver 0.0.0.0:80
+/env/bin/python /opt/boundless/exchange/manage.py runserver 0.0.0.0:8000
