0902b5f45f47457235872f18a7d5a6b03c78bee2
Daniel Berry <dberry@boundlessgeo.com>
2017-02-22 18:48:37 -0600

    moved the exchange tests to the container

---
+++ docker/travis/pytests.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+source $DIR/common.sh
+
+# set test settings
+export DJANGO_SETTINGS_MODULE='exchange.settings'
+export PYTEST=1
+
+# migrate and collectstatic
+$CMD migrate
+$CMD collectstatic --noinput
+cd /opt/boundless/exchange
+
+# run tests
+PYTEST=True bash -c '/env/bin/py.test --cov exchange exchange/tests/'
