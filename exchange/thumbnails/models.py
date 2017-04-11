def415a5236e590f617df15002a8574433634dcc
Dan 'Ducky' Little <theduckylittle@gmail.com>
2017-04-11 15:54:27 -0500

    Added Signals for GeoServer Auto-Thumbnails.
    1. Added code to check for a thumbnail in the database.
    2. Added the ability to generate a thumbnail from Geoserver.
       It's a simplified version of the GeoServer code.

--- exchange/thumbnails/models.py
+++ exchange/thumbnails/models.py
@@ -3,6 +3,12 @@
 #
 
 from django.db import models
+from django.db.models.signals import post_save
+
+from geonode.layers.models import Layer
+from geonode.maps.models import Map
+
+from . import get_gs_thumbnail
 
 
 class Thumbnail(models.Model):
@@ -15,3 +21,42 @@ class Thumbnail(models.Model):
 
     class Meta:
         unique_together = ('object_type', 'object_id')
+
+
+# Check to see if an object already has a thumbnail.
+def has_thumbnail(objectType, objectId):
+    try:
+        Thumbnail.objects.get(object_type=objectType, object_id=objectId)
+        return True
+    except Thumbnail.DoesNotExist:
+        return False
+
+
+# This is used as a post-save signal that will
+# automatically geneirate a new thumbnail if none existed
+# before it.
+def generate_thumbnail(instance, sender, **kwargs):
+    object_id = None
+    obj_type = None
+    if(instance.class_name == 'Map'):
+        object_id = instance.id
+        obj_type = 'maps'
+    elif(instance.class_name == 'Layer'):
+        object_id = instance.typename
+        obj_type = 'layers'
+
+    if(object_id is not None and not has_thumbnail(obj_type, object_id)):
+        # have geoserver generate a preview png and return it.
+        thumb_png = get_gs_thumbnail(instance)
+
+        if(thumb_png is not None):
+            # create a new thumbnail.
+            thumb = Thumbnail(object_type=obj_type, object_id=object_id,
+                              thumbnail_mime='image/png',
+                              thumbnail_img=thumb_png)
+            thumb.save()
+
+
+# add this APIs signals for saving things.
+post_save.connect(generate_thumbnail, sender=Layer)
+post_save.connect(generate_thumbnail, sender=Map)
