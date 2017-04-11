d50a5476fa766d86c140ee4b310e08d7617fe3d1
Dan 'Ducky' Little <theduckylittle@gmail.com>
2017-04-11 11:51:00 -0500

    Changed object types to support plurals.
    The application uses "maps", "layers", "documents" in the URLs.
    Allowing the plurals makes it easier to form the thumbnail URLs.

--- exchange/thumbnails/urls.py
+++ exchange/thumbnails/urls.py
@@ -4,6 +4,6 @@ from django.conf.urls import url
 from .views import thumbnail_view
 
 urlpatterns = (
-    url(r'^thumbnails/(?P<objectType>map|document|layer)/(?P<objectId>.+)$',
+    url(r'^thumbnails/(?P<objectType>maps|documents|layers)/(?P<objectId>.+)$',
         thumbnail_view, name='thumbnail_handler'),
 )
