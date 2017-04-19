184cf259e29347ccca4bda939e2beb4470f2ad54
Dan 'Ducky' Little <theduckylittle@gmail.com>
2017-04-19 16:38:25 -0500

    Added test to prove auth changes for csw views.
    1. Adapted ExchangeTest to allow logging in as test or
       as admin.
    2. Added permissions tests in csw_perms_test.py that test
       whether those views can be accessed by non-staff users.

---
+++ exchange/tests/csw_perms_test.py
@@ -0,0 +1,34 @@
+#
+# Ensure only administrative users can
+# access the CSW views.
+#
+
+from . import ExchangeTest
+
+
+class CswPermissionsTest(ExchangeTest):
+
+    def _test_url(self, url, expectedStatus):
+        r = self.client.get(url)
+        self.assertEqual(r.status_code, expectedStatus,
+                         'Mismatched status for %s. Expected %d, got %d' % (
+                             url, expectedStatus, r.status_code
+                         ))
+
+    def test_as_test(self):
+        # login as test.
+        self.login(asTest=True)
+
+        # should get a redirect to the authentication screen.
+        self._test_url('/csw/new/', 302)
+        self._test_url('/csw/status/', 302)
+        self._test_url('/csw/status_table/', 302)
+
+    def test_as_admin(self):
+        # login as admin
+        self.login()
+
+        # these should all clear as 200s
+        self._test_url('/csw/new/', 200)
+        self._test_url('/csw/status/', 200)
+        self._test_url('/csw/status_table/', 200)
