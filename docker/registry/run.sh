dacf45582ebac6b84bfd7cf21543278568f201fd
david.w.bitner <david.w.bitner@noaa.gov>
2017-07-06 13:57:26 -0500

    remove changes used just for dev

--- docker/registry/run.sh
+++ docker/registry/run.sh
@@ -5,5 +5,5 @@ source /etc/profile.d/vendor-libs.sh
 source /env/bin/activate
 
 cd /opt/registry
-pip install -r requirements.txt
+
 python registry.py runserver 0.0.0.0:8001
--- docker/registry/setupdb.sh
+++ docker/registry/setupdb.sh
@@ -5,6 +5,5 @@ source /etc/profile.d/vendor-libs.sh
 source /env/bin/activate
 
 cd /opt/registry
-pip install requirements.txt
 
 python registry.py pycsw -c setup_db
