c822062222750bc1ef3cdc31dc8df7927f4bb87f
travis <travislbrundage@gmail.com>
2017-12-01 13:59:58 -0800

    Fix test script to run all tests by default

--- dev/run_tests.sh
+++ dev/run_tests.sh
@@ -8,6 +8,5 @@ export PYTEST=1
 
 python manage.py migrate
 python manage.py collectstatic --noinput
-#py.test --ignore=tests/ --cov-report html:cov_html --cov=exchange exchange/tests/
-py.test -v --ignore=tests/ --cov=exchange exchange/tests/celery_test.py
+py.test --ignore=tests/ --cov-report html:cov_html --cov=exchange exchange/tests/
 coverage report -m
