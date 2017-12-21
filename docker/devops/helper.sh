2ad172f80a4401671e1c77b5fcf8f19951daa07a
Daniel Berry <dberry@boundlessgeo.com>
2017-12-21 12:49:33 -0600

    adjustments for sonar stage (master) (#494)
    adjust sed regex

--- docker/devops/helper.sh
+++ docker/devops/helper.sh
@@ -10,17 +10,22 @@ function lint {
 
 # Jenkins specific function for builds on master branch, requires sonar auth token
 function sonar-scan {
-    for f in `find . -type f -name "setup.py"`  ; do (pushd `dirname $f` \
-       && NAME=`grep name setup.py | cut -f2 -d'=' | sed "s|[',]||g"` \
-       && sonar-scanner -Dsonar.host.url=$SONAR_HOST_URL \
-                        -Dsonar.login=$SONAR_TOKEN \
-                        -Dsonar.projectKey=com.boundlessgeo.bex:$NAME \
-                        -Dsonar.sources=. \
-                        -Dsonar.projectVersion=1.0 \
-                        -Dsonar.projectName=$NAME \
-                        -Dsonar.language=py \
-                        -Dsonar.python.pylint=/usr/bin/pylint \
-       && popd \
-       || popd); done
-    mvn clean install sonar:sonar -Dsonar.host.url=$SONAR_HOST_URL -Dsonar.login=$SONAR_TOKEN
+    exchange_ver=`grep "__version__ =" exchange/__init__.py | sed "s/__version__ = '\(.*\)'/\1/"`
+    echo $exchange_ver
+    sonar-scanner -Dsonar.host.url=$SONAR_HOST_URL \
+              -Dsonar.login=$SONAR_TOKEN \
+              -Dsonar.projectKey=com.boundlessgeo.exchange \
+              -Dsonar.sources=exchange \
+              -Dsonar.projectVersion=$exchange_ver \
+              -Dsonar.projectName=exchange \
+              -Dsonar.language=py \
+              -Dsonar.python.pylint=/usr/bin/pylint
+    sonar-scanner -Dsonar.host.url=$SONAR_HOST_URL \
+              -Dsonar.login=$SONAR_TOKEN \
+              -Dsonar.projectKey=com.boundlessgeo.geonode \
+              -Dsonar.sources=vendor/geonode/geonode \
+              -Dsonar.projectVersion=2.6.1 \
+              -Dsonar.projectName=geonode \
+              -Dsonar.language=py \
+              -Dsonar.python.pylint=/usr/bin/pylint
 }
